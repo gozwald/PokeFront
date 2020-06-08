a64ad75f08f4b741761f911b716c7d7843a124ca
Samuel Greenwald <gozwald@gmail.com>
2020-06-08 14:31:24 +0200

    names displayed!

--- src/Pokemonlist.js
+++ src/Pokemonlist.js
@@ -5,20 +5,19 @@ import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    padding: theme.spacing(1),
+    padding: theme.spacing(3),
     textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
 
 export default function PokemonList({ list }) {
-  console.log(list);
   const classes = useStyles();
 
   return (
     <>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>item</Paper>
+      <Grid item xs={1}>
+        <Paper className={classes.paper}>{list.name.english} </Paper>
       </Grid>
     </>
   );
